e9b55d906170373217487a51245b1f0c9c19ab96
Wendy You <wyou130@gmail.com>
2023-01-27 16:23:38 -0500

    Added update route, update action, and private methods for params and for ids that do not exist. Tested in Postman.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,27 @@
 class FoodsController < ApplicationController
 
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
     # returns all food instances
     def index
         render json: Food.all, status: :ok
+    end
+
+    # edit food instance by its id
+    def update
+        food = Food.find(params[:id])
+        food.update!(food_params)
+        render json: food, status: :accepted
+    end
+
+    private
+
+    def food_params
+        params.permit(:name, :price, :category, :add_ons, :details)
+    end
+
+    def not_found
+        render json: { error: 'Food not found' }, status: :not_found
     end
 
 end
