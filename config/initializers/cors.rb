b362d95294121ee39d032b58332d34433afe46f5
glennanj1 <glennan20@gmail.com>
2023-02-02 12:11:42 -0500

    added credentials to initializer/cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3006', 'https://masterpeacegrill.netlify.app', 'https://www.masterpeacegrill.com'
+    origins 'http://localhost:3001', 'https://masterpeacegrill.netlify.app', 'https://www.masterpeacegrill.com'
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
