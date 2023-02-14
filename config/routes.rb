8277481ef5deebe7df0cd0e17f8559700f35f717
Wendy You <wyou130@gmail.com>
2023-02-14 14:13:32 -0500

    Created Category resource and added a couple example seeds. Next need to update Food table with foreign key.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  resources :categories
   resources :foods, only: [:index, :update]
   resources :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_201103) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_190831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "add_ons"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
