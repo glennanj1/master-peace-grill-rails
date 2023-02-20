29ae53c28dd749a4a23981c728cd39292d5b2143
glennanj1 <glennan20@gmail.com>
2023-02-19 23:45:18 -0500

    removed expire time using application.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,10 @@
 class SessionsController < ApplicationController
     skip_before_action :authorize, only: [:create]
-    include ::ActionController::Cookies
 
     def create
         user = User.find_by(email: params[:email])
 
         if user && user.authenticate(params[:password])
-            $expire_time = Time.now + 10.minutes
-
             session[:user_id] = user.id
             render json: user
         else
