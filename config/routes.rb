624d1a1545034616284c92b648548786ade0c059
glennanj1 <glennan20@gmail.com>
2023-02-26 20:11:18 -0500

    updated category route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :categories
+  resources :categories, only: [:index, :update, :show]
   resources :foods, only: [:index, :update, :show]
   resources :users
 
