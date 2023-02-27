de8c169ab7e30b1fa1266b3dea2b59bb35542d98
glennanj1 <glennan20@gmail.com>
2023-02-26 20:10:40 -0500

    category update show and private methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,31 @@
 class CategoriesController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+
     def index
         render json: Category.all, status: :ok
     end
+
+    # edit food instance by its id
+    def update
+        category = Category.find(params[:id])
+        category.update!(category_params)
+        render json: category, status: :accepted
+    end
+
+    def show 
+        category = Category.find(params[:id])
+        render json: category, status: :ok
+    end
+
+    private
+
+    def category_params
+        params.permit(:id, :name, :description, :add_ons, :add_ons)
+    end
+
+    def not_found
+        render json: { error: 'Category not found' }, status: :not_found
+    end
+
+
 end
