d05c9acea8d0a6926aba7a6069265a83f8110809
glennanj1 <glennan20@gmail.com>
2023-02-26 20:12:14 -0500

    foods update

--- db/migrate/20230116155204_create_foods.rb
+++ db/migrate/20230116155204_create_foods.rb
@@ -2,7 +2,7 @@ class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
       t.string :name
-      t.float :price
+      t.decimal :price, :precision => 10, :scale => 2
       t.string :category
       t.text :add_ons
       t.text :details
