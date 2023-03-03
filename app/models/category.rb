d6b3dd6ab44f9034051059272fff5a43eefc5b94
Brooke Ross <brooke.ross2800@gmail.com>
2023-03-02 17:30:10 -0700

    added validations for food, category,

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
     has_many :foods
+
+    validates :name, length:{ maxiumum: 65,
+    too_long: "%{count} characters is the maxiumum allowed"}
+
+    validates :description, :add_ons, length: { maximum: 200}
 end
